7e53e0949038787ef62bbf8b2ab2a2be5f7cc991
YoonSeok Hong <yshong93@outlook.com>
2016-04-26 03:41:51 +0900

    Answer FlappyBird

---
+++ Answer/FlappyBirdClone-master/FlappyBirdClone-master/Assets/Scripts/FacebookLoginScript.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using Assets.Scripts.CloudBread;
+using JsonFx.Json;
+using UnityEngine.SceneManagement;
+
+public class FacebookLoginScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Awake function from Unity's MonoBehavior
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    private void LoginwithPermissions()
+    {
+        var perms = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(perms, AuthCallback);
+    }
+
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            // AccessToken class will have session details
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log(aToken.UserId);
+            // Print current access token's granted permissions
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+
+            // 유저 이름(닉네임) 불러오기
+            FB.API("me?fields=name", HttpMethod.GET, NameCallBack);
+
+
+            // 인증 토큰 가져오기
+            CloudBread cb = new CloudBread();
+            cb.Login(AzureAuthentication.AuthenticationProvider.Facebook, aToken.TokenString, Callback_login);
+        }
+        else {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+    private void Callback_login(string id, WWW www)
+    {
+        print(www.text);
+        string resultJson = www.text;
+
+        JsonReader jsonReader = new JsonReader();
+        AuthData resultData = jsonReader.Read<AuthData>(resultJson);
+
+        AzureMobileAppRequestHelper.AuthToken = resultData.authenticationToken;
+
+
+
+        PlayerPrefs.SetString("userId", resultData.user.userId);
+
+        CloudBread cb = new CloudBread();
+        cb.CBInsRegMember(Callback_CBInsRegMember);
+
+    }
+
+    public void Callback_CBInsRegMember(string id, WWW www)
+    {
+        if (www.error != null) //새로 회원 가입
+        {
+            print("이미 가입된 회원");
+            StartGame();
+
+
+        }
+        else //이미 가입된 회원
+        {
+            print("새로 가입한 회원");
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("mainGame");
+    }
+
+    private void NameCallBack(IGraphResult result)
+    {
+        string userName = (string)result.ResultDictionary["name"];
+        print(userName + "님 안녕하세요^^");
+        PlayerPrefs.SetString("nickName", userName);
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+    public void FacebookLoginButtonClicked()
+    {
+        if (!FB.IsLoggedIn)
+            LoginwithPermissions();
+    }
+}
---
+++ Answer/FlappyBirdClone-master/FlappyBirdClone-master/Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.CloudBread;
+
+public class ScoreManagerScript : MonoBehaviour {
+
+    public static int Score { get; set; }
+
+    private bool deadRefreshFlag = true;
+
+    // Use this for initialization
+    void Start () {
+        (Tens.gameObject as GameObject).SetActive(false);
+        (Hundreds.gameObject as GameObject).SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (previousScore != Score) //save perf from non needed calculations
+        {
+            if (Score < 10)
+            {
+                //just draw units
+                Units.sprite = numberSprites[Score];
+            }
+            else if (Score >= 10 && Score < 100)
+            {
+                (Tens.gameObject as GameObject).SetActive(true);
+                Tens.sprite = numberSprites[Score / 10];
+                Units.sprite = numberSprites[Score % 10];
+            }
+            else if (Score >= 100)
+            {
+                (Hundreds.gameObject as GameObject).SetActive(true);
+                Hundreds.sprite = numberSprites[Score / 100];
+                int rest = Score % 100;
+                Tens.sprite = numberSprites[rest / 10];
+                Units.sprite = numberSprites[rest % 10];
+            }
+        }
+        else if (GameStateManager.GameState == GameState.Dead)
+        {
+            int bestScore = 0;
+
+            if (PlayerPrefs.HasKey("bestScore"))
+            {
+                bestScore = PlayerPrefs.GetInt("bestScore");
+            }
+            else
+            {
+                PlayerPrefs.SetInt("bestScore", bestScore);
+            }
+
+            if (bestScore >= Score)
+            {
+            }
+            else
+            {
+                PlayerPrefs.SetInt("bestScore", Score);
+
+                if (deadRefreshFlag)
+                {
+                    deadRefreshFlag = false;
+
+                    // 사용자 게임 인포 업데이트
+                    CloudBread cb = new CloudBread();
+                    cb.CBComUdtMemberGameInfoes(Callback_Success);
+                }
+            }
+        }
+    }
+    public void Callback_Success(string id, WWW www)
+    {
+        
+        // 랭킹 업데이트
+        CloudBread cb = new CloudBread();
+        cb.CBRank(Callback_CBRank);
+    }
+
+    public void Callback_CBRank(string str, WWW www)
+    {
+        print("CBRank Success");
+        print(str);
+    }
+
+
+
+    int previousScore = -1;
+    public Sprite[] numberSprites;
+    public SpriteRenderer Units, Tens, Hundreds;
+}
